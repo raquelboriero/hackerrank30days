e6d9691efc7cc647a8cf0f1182354e7a25ea0cb1
Raquel Carvalho <60790005+raquelboriero@users.noreply.github.com>
2022-07-26 16:58:50 +0100

    day10 : o que fiz

--- HackerRank/DaysCode/Day10.cs
+++ HackerRank/DaysCode/Day10.cs
@@ -29,42 +29,27 @@ namespace HackerRank.DaysCode
 
             var numberBase10 = Convert.ToInt32(this.console.ReadLine().Trim());
             var numberBinary = Convert.ToString(numberBase10, 2);
+            numberBinary = numberBinary.Substring(0, numberBinary.Length);
 
-            //var result = 0;
-
-            //foreach (var item in numberBinary)
-            //{
-
-            //    var binary1 = 1;
-            //    switch (item)
-            //    {
-            //        case binary1:
-            //            result =+ result;
-            //            break;
-            //        case 0:
-            //            break;
-            //    }
-
-            //}
 
             var result = 0;
 
             for (int i = 0; i < numberBinary.Length; i++)
             {
-                if (i == 0)
+                if (numberBinary[i] == 1)
                 {
+                    result += numberBinary[i];
                     continue;
                 }
 
-                while (i == 1)
-                {
-                    result = +result;
+                else
+                { 
+                    break;
                 }
 
 
             }
 
- 
 
 
 
